9829348c067cdc195216a881d6d58129cc90cf4e
alidezmo <96472790+alidezmo@users.noreply.github.com>
2026-04-07 19:19:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,12 @@
-const CACHE_NAME = 'mg-home-chat-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-// تثبيت ملفات الكاش الأساسية
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-// جلب البيانات من الكاش عند انقطاع الإنترنت
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+self.addEventListener('fetch', (e) => {
+  // هذا الكود يخبر المتصفح أن التطبيق جاهز للعمل بدون إنترنت
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
   );
 });
