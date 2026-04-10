ea78d7a1f77a7fa7b0568c63f2ea57832593e14f
alidezmo <96472790+alidezmo@users.noreply.github.com>
2026-04-10 21:43:17 +0200

    Create state.js

---
+++ state.js
@@ -0,0 +1,17 @@
+export const state = {
+    myName: localStorage.getItem('chat_username') || '',
+    myUserId: localStorage.getItem('chat_userid') || '',
+    myRole: localStorage.getItem('chat_role') || 'user',
+    currentChatMode: 'global',
+    currentChatTargetId: null,
+    currentMessagesRefPath: '',
+    currentListeners: [],
+    typingListener: null,
+    replyingToMsg: null,
+    pendingUnreadMessages: [],
+    uploadingKeys: [],
+    pendingAttachment: null,
+    appStartTime: Date.now(),
+    unreadCounts: { global: 0 },
+    trackedRooms: new Set()
+};
