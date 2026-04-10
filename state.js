b7f0626938c5798076a48118e6661e6c15959ed0
alidezmo <96472790+alidezmo@users.noreply.github.com>
2026-04-10 22:30:02 +0200

    Update state.js

--- state.js
+++ state.js
@@ -15,11 +15,11 @@ export const state = {
     unreadCounts: { global: 0 },
     trackedRooms: new Set(),
     
-    // متغيرات التمرير اللانهائي
-    oldestMessageKey: null,
-    isLoadingMore: false,
-    allMessagesLoaded: false, // <-- الفاصلة هنا كانت هي سبب المشكلة!
+    // --- متغيرات التمرير اللانهائي (الجديدة) ---
+    oldestMessageKey: null, // مفتاح أقدم رسالة تم تحميلها
+    isLoadingMore: false,   // لمنع تكرار التحميل إذا كان يحمل بالفعل
+    allMessagesLoaded: false,
+
+    totalUsers: 0
     
-    // عدد المستخدمين
-    totalUsers: 0 
 };
