57083ad59aefdfe82ccc955b7eb0b0ac12a647fe
alidezmo <96472790+alidezmo@users.noreply.github.com>
2026-04-10 22:44:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -38,16 +38,16 @@ window.startApp = function() {
     window.listenForNotifications('messages_global', 'global', 'global'); 
     window.switchChat('global', 'المجموعة العامة');
 
-  window.OneSignalDeferred = window.OneSignalDeferred || [];
+ window.OneSignalDeferred = window.OneSignalDeferred || [];
     OneSignalDeferred.push(async function(OneSignal) {
-        // 👇 هذا الكود يجلب الرابط الكامل للموقع (مثال: https://alidezmo.github.io/MG/)
-        const currentUrl = window.location.href.split('?')[0].split('#')[0]; // تنظيف الرابط
-        const basePath = currentUrl.endsWith('/') ? currentUrl : currentUrl.substring(0, currentUrl.lastIndexOf('/') + 1);
+        // 👇 هذا الكود يجلب "اسم المجلد" فقط بدون (https)، مثال: (/MG/)
+        const pathname = window.location.pathname;
+        const basePath = pathname.substring(0, pathname.lastIndexOf('/') + 1);
 
         await OneSignal.init({ 
             appId: "c89a2d04-de43-42eb-85b3-2f45c47b6b08", 
             safari_web_id: "web.onesignal.auto.1afe2633-50cf-455e-8f3e-a50d8cbe1d12", 
-            // 👇 نوجه OneSignal للمسار الكامل بشكل صريح لا يقبل الخطأ
+            // 👇 نوجه OneSignal للمجلد بشكل نظيف ومقبول
             serviceWorkerPath: basePath + "sw.js",
             serviceWorkerParam: { scope: basePath }
         });
